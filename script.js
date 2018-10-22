68d59f423b47dd238b5c6c8e426134c8928a6c88
rebeccahongsf <rebeccahongsf@gmail.com>
2018-10-21 22:52:31 -0700

    second commit

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-$(document).ready(function() {
-  // activates popover effect on skillset
-  $(function () {
-    $('[data-toggle="popover"]').popover();
-  });
-
-  $('a[href^="#"]').on('click', function(e) {
-   e.preventDefault();
-
-   // store hash
-   var hash = this.hash;
-   console.log(hash);
-
-   $('body').animate({
-     scrollTop: $(hash).offset().top
-   }, 800, function() {
-
-     // when done, add hash to url
-     // (default click behaviour)
-     window.location.hash = hash;
-   });
-
-});
-});
+$(document).on('click',function(){
+$('.collapse').collapse('hide');
+})
